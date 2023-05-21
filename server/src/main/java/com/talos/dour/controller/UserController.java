bc05f6833a2dfa3c73b7928d0002ac1bf72c667b
Md. Shahriar Islam Bhuiyan <shojeb.bhuiyan@gmail.com>
2023-05-21 18:36:50 +0600

    User get by id

--- server/src/main/java/com/talos/dour/controller/UserController.java
+++ server/src/main/java/com/talos/dour/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.talos.dour.controller;
 
+import com.talos.dour.exception.ResourceNotFoundException;
 import com.talos.dour.model.User;
 import com.talos.dour.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,5 +23,12 @@ public class UserController {
     @PostMapping("/add")
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found!"));
+        return ResponseEntity.ok(user);
     }
 }
